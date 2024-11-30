5c0c3fed0cbc8fcc1d3d0ee84cab8d5cdcc9e2c8
Henry Greger <hgrege3@wgu.edu>
2024-11-30 11:43:42 -0600

    Update pipeline for test_regression_model

--- main.py
+++ main.py
@@ -116,12 +116,15 @@ def go(config: DictConfig):
                         )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # Run the regression model on the test data
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                parameters = {
+                     "mlflow_model": "random_forest_export:prod",
+                     "test_dataset": "test_data.csv:latest"
+                }
+            )
 
 
 if __name__ == "__main__":
